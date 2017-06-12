800847369503984555435faa636ad6611ceebcce
Michal <m14lickiewicz@gmail.com>
2017-06-12 20:34:06 +0200

    Print toppings on pages, styling input.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,31 @@
-const pizzaPortal = angular.module('pizzaPortal', ['pizzaDirectives']);
+const pizzaPortal = angular.module('pizzaPortal', []);
 
 pizzaPortal.controller('pizzaCrtl', ['$scope', '$interval', ($scope, $interval) => {
+
+    $scope.toppings = [
+        {name: 'Bacon'},
+        {name: 'Basil'},
+        {name: 'Chili'},
+        {name: 'Mozzarella'},
+        {name: 'Mushroom'},
+        {name: 'Olive'},
+        {name: 'Onion'},
+        {name: 'Pepper'},
+        {name: 'Pepperoni'},
+        {name: 'Sweetcorn'},
+        {name: 'Tomato'},
+        {name: 'Salami'}
+    ];
 
     $interval(() => {
         $scope.myTime = new Date();
 
     }, 1000);
+
     $scope.isChecked = false;
+
+    $scope.addToppings = () => {
+        console.log('nieeeee')
+    }
+
 }]);
