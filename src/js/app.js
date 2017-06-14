248e84929266d2fd325fb8e27a6d5e7b7f65d0c4
Michal <m14lickiewicz@gmail.com>
2017-06-14 17:15:45 +0200

    Styling order summary and set  counting topping prices function in progresss.

--- src/js/app.js
+++ src/js/app.js
@@ -34,16 +34,29 @@ pizzaPortal.controller('pizzaCrtl', ['$scope', '$interval', ($scope, $interval) 
 
     $scope.isChecked = false;
 
+    $scope.setToppingPrice = (event) => {
+        if (event.currentTarget.value == 1) {
+            $scope.toppingPrice = 1.3;
+        } else if (event.currentTarget.value == 2) {
+            $scope.toppingPrice = 1;
+        } else {
+            $scope.toppingPrice = 0.7
+        }
+        console.log($scope.toppingPrice)
+    };
+
     $scope.myToppings = new Array;
 
-    $scope.isCheckedTopping = (event, name)=>{
+    $scope.isCheckedTopping = (event, name) => {
+        console.log(event.currentTarget.value);
         console.log(event.currentTarget.checked ? 'aaaaa' : 'a');
-        if(event.currentTarget.checked) {
+        if (event.currentTarget.checked) {
             $scope.myToppings.push({name: name});
         } else {
-            $scope.myToppings.splice( $scope.myToppings.indexOf(name),1);
+            $scope.myToppings.splice($scope.myToppings.indexOf(name), 1);
         }
 
         console.log($scope.myToppings)
     }
+
 }]);
